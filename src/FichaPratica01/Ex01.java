b13ca0111cd693387bb3f4e185bcf4b52543239b
Juan <juanpereiracosta@gmail.com>
2024-08-02 22:27:26 +0100

    Projeto IntelliJ adicionado.

---
+++ src/FichaPratica01/Ex01.java
@@ -0,0 +1,28 @@
+package FichaPratica01;
+
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+
+        // Import de Scanner - sempre que queremos ler dados do teclado
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int num1, num2, soma;
+
+        // Ler num1
+        System.out.print("Introduza um numero: ");
+        num1 = input.nextInt();
+
+        // Ler num2
+        System.out.print("Introduza outro numero: ");
+        num2 = input.nextInt();
+
+        // Calcular a soma
+        soma = num1+num2;
+
+        // Apresentar a soma
+        System.out.println("Soma: "+soma);
+    }
+}
